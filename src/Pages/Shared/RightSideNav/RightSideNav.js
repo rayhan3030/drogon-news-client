a7ca86516537ce97d3351ca08a21e4e28c7f3faf
rayhan3030 <rayhanddd@gmail.com>
2022-10-19 13:28:03 +0600

    ending push for module 60

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch, FaWpforms } from "react-icons/fa"
+import ListGroup from 'react-bootstrap/ListGroup';
+import BrandCarousel from '../BrandCarousel/BrandCarousel';
+
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>Right Side Nav</h2>
+            <ButtonGroup vertical>
+                <Button className='mb-2' variant='outline-primary'> <FaGoogle />  Login with Google</Button>
+                <Button variant='outline-dark'> <FaGithub></FaGithub> Login with Github</Button>
+            </ButtonGroup>
+            <div className='mt-5'>
+                <h5 className='mb-3'>Find us On</h5>
+                <ListGroup className=''>
+                    <ListGroup.Item className='mb-2'><FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWhatsapp /> Whatsapp</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitch /> Twitch</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWpforms /> Terms & Conditions</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <div>
+                <BrandCarousel />
+            </div>
         </div>
     );
 };

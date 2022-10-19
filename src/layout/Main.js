f3e2093cf2e633794a2d7096d81b93c9de564035
rayhan3030 <rayhanddd@gmail.com>
2022-10-19 10:47:30 +0600

    Component Folder tree  created

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
-            <Outlet></Outlet>
+            <Container>
+                <Row>
+                    <Col lg="2">
+                        <h2>Side Nav</h2>
+                    </Col>
+                    <Col lg="7">
+                        <Outlet></Outlet>
+                    </Col>
+                    <Col lg="3">
+                        <h2> Right Side Nav</h2>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
